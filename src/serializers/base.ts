a1149572feb38e451c377e3815b23b25140342d9
Akshay Kumar <dhupamakshay.kumar@gmail.com>
2021-09-24 12:11:19 +0530

    Move the common code of serializers to Base class

---
+++ src/serializers/base.ts
@@ -0,0 +1,39 @@
+import Lib from "jsonapi-serializer";
+import HttpError from "../errors/http-error";
+import { DeserialisedBodyMap, SerializedBodyMap } from "../types/IntentBody";
+
+export default class Serializer {
+  type: string = '';
+  attributes: Array<string> = [];
+  relationships = {};
+
+  serialize = <k extends keyof SerializedBodyMap>(
+    _kind: k,
+    data: SerializedBodyMap[k],
+    meta = {}
+  ): Promise<JSON> => {
+    try {
+      const Serializer = new Lib.Serializer(this.type, {
+        attributes: this.attributes,
+        meta,
+        ...this.relationships,
+      });
+      return Serializer.serialize(data);
+    } catch (e) {
+      throw new HttpError(500, "Internal Server Error");
+    }
+  };
+
+  deserialize = <k extends keyof DeserialisedBodyMap>(
+    _kind: k,
+    data: JSON
+  ): Promise<DeserialisedBodyMap[k]> => {
+    try {
+      return new Lib.Deserializer({
+        keyForAttribute: "camelCase",
+      }).deserialize(data);
+    } catch (e) {
+      throw new HttpError(500, "Internal Server Error");
+    }
+  };
+}
--- src/serializers/intent.ts
+++ src/serializers/intent.ts
@@ -1,39 +1,6 @@
-import Lib from "jsonapi-serializer";
-import HttpError from "../errors/http-error";
-import { DeserialisedBodyMap, SerializedBodyMap } from "../types/IntentBody";
+import Base from './base'
 
-export default class Serializer {
-  type = "reply";
-  attributes = ["reply"];
-  relationships = {};
-
-  serialize = <k extends keyof SerializedBodyMap>(
-    _kind: k,
-    data: SerializedBodyMap[k],
-    meta = {}
-  ): Promise<JSON> => {
-    try {
-      const Serializer = new Lib.Serializer(this.type, {
-        attributes: this.attributes,
-        meta,
-        ...this.relationships,
-      });
-      return Serializer.serialize(data);
-    } catch (e) {
-      throw new HttpError(500, "Internal Server Error");
-    }
-  };
-
-  deserialize = <k extends keyof DeserialisedBodyMap>(
-    _kind: k,
-    data: JSON
-  ): Promise<DeserialisedBodyMap[k]> => {
-    try {
-      return new Lib.Deserializer({
-        keyForAttribute: "camelCase",
-      }).deserialize(data);
-    } catch (e) {
-      throw new HttpError(500, "Internal Server Error");
-    }
-  };
+export default class Serializer extends Base {
+  type = 'reply'
+  attributes = ['reply']
 }
